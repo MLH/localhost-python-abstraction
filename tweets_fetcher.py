21f150d7f92915f9cc6c37602aa78e701cf30fdf
Halisson Bruno <halissonvit@gmail.com>
2019-05-07 11:03:39 -0300

    Paginate through results to fetch 3200 rather than 200

--- tweets_fetcher.py
+++ tweets_fetcher.py
@@ -1,12 +1,33 @@
 import config
 import tweepy
 
+MAX_NUMBER_OF_TWEETS_PER_PAGE = 200
+MAX_NUMBER_OF_TWEETS_ALLOWED = 3200
+
 # user authentication to use twitter API
 def get_user_auth_twitter():
     auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
     return auth
+
+
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except StopIteration:
+            return
+        except tweepy.RateLimitError:
+            time.sleep(15 * 60)
+
+
+def get_number_of_pages():
+    number_of_tweets_config = int(config.NUM_TWEETS_TO_GRAB or "1000")
+    max_number_of_tweets_allowed = min(
+        MAX_NUMBER_OF_TWEETS_ALLOWED, number_of_tweets_config
+    )
+    return max_number_of_tweets_allowed / MAX_NUMBER_OF_TWEETS_PER_PAGE
 
 
 # fetch tweets
@@ -20,8 +41,17 @@ def get_tweets(twitter_handle):
 
     auth = get_user_auth_twitter()
     api = tweepy.API(auth)
-    public_tweets = api.user_timeline(
-        twitter_handle, count=(config.NUM_TWEETS_TO_GRAB or 1000), tweet_mode="extended"
-    )
+
+    public_tweets = []
+
+    for tweets in limit_handled(
+        tweepy.Cursor(
+            api.user_timeline,
+            twitter_handle,
+            tweet_mode="extended",
+            count=MAX_NUMBER_OF_TWEETS_PER_PAGE,
+        ).pages(get_number_of_pages())
+    ):
+        public_tweets.extend(tweets)
 
     return [tweet._json["full_text"] for tweet in public_tweets]
